f7bd516adb47e36a7c638d4c276b7c8e8ea74f48
sigouinb <sigouinb@educ.college-montreal.qc.ca>
2022-09-26 16:29:53 -0400

    tp2

--- tp2.py
+++ tp2.py
@@ -5,14 +5,15 @@ from random import randint
 print("J'ai choisi un nombre entre 1 et 100. À vous de deviner.")
 
 nombre = randint(0,100)
-guess = int(input(("Entre un nombre.")))
-if guess == nombre:
-    print("Tu as bien deviné!")
+guess = int(input("Entre un nombre."))
 
-
-elif guess > nombre:
-    print("Incorrect. Ton choix est plus grand que le nombre que j'ai choisi." + guess)
-
-
-elif guess < nombre:
-    print("Incorrect. Ton choix est plus petit que le nombre que j'ai choisi." + guess)
+def(game):
+    if guess == nombre:
+        guess = True
+        print("Tu as bien deviné!")
+    elif guess != nombre:
+        guess = False
+        print("Mauvaise réponse! Essaye encore.")
+while guess == False:
+    guess = int(input("Entre un nombre."))
+    run(game)
