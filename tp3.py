5ce5a923a3fd78968b1cf23dcfb0a3d50d6c6e68
sigouinb <sigouinb@educ.college-montreal.qc.ca>
2022-10-26 10:45:02 -0400

    tp3

---
+++ tp3.py
@@ -0,0 +1,19 @@
+#Benjamin Sigouin, 404
+#TP3
+
+
+
+def adversaire():
+    choix == str(input("""Vous tombez face à un monstre. Vous avez 4 options: 
+    1. Combattre"
+    2. Contourner
+    3. Afficher les règles du jeu
+    4. Quitter la partie"""))
+    if choix = "1":
+
+    elif choix = "2":
+
+    elif choix = "3":
+
+    elif choix = "4":
+
