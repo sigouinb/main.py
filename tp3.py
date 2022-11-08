580e6630273b703d323b69f8c5e23ae3eac39291
sigouinb <sigouinb@educ.college-montreal.qc.ca>
2022-11-08 09:18:41 -0500

    tp3

--- tp3.py
+++ tp3.py
@@ -1,19 +1,35 @@
 #Benjamin Sigouin, 404
 #TP3
 
+from random import randint
 
-
-def adversaire():
-    choix == str(input("""Vous tombez face à un monstre. Vous avez 4 options: 
+def jeu():
+    choix = str(input("""Vous tombez face à un monstre. Vous avez 4 options: 
     1. Combattre"
     2. Contourner
     3. Afficher les règles du jeu
     4. Quitter la partie"""))
-    if choix = "1":
+    if choix == "1":
+        def adversaire():
+            global winstreak
+            global playerhp
+            winstreak = 0
+            enemyhp = 4
+            playerhp = 20
+            dice = randint(1, 6)
+            print("Vous tombez face à un adversaire.")
+            print("Vous avez roulé", dice)
+            if dice > enemyhp:
+                winstreak += 1
+                print("Vous avez gagné. Votre nombre de victoires consécutives est", winstreak)
+                print("Points de vie:", playerhp + dice)
+                jeu()
+            elif dice < enemyhp:
+                print("Vous avez perdu.")
+                print("Points de vie:", playerhp - dice)
+                jeu()
 
-    elif choix = "2":
+        adversaire()
 
-    elif choix = "3":
 
-    elif choix = "4":
-
+jeu()
