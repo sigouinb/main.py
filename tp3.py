db5729eac1e29733745b67c5d1c210cf3b446dc2
sigouinb <sigouinbeduc.college-montreal.qc.ca>
2022-11-22 09:20:39 -0500

    tp3

--- tp3.py
+++ tp3.py
@@ -3,21 +3,28 @@
 
 from random import randint
 
+playerhp = 20
+enemyStrength = randint(1,5)
 def jeu():
-    choix = str(input("""Vous tombez face à un monstre. Vous avez 4 options: 
-    1. Combattre"
-    2. Contourner
-    3. Afficher les règles du jeu
-    4. Quitter la partie"""))
+    monsternumber = 0
+    global monsternumber
+    print("Vous tombez face à un monstre de difficulté ", enemyStrength, ". Vous avez 4 options:")
+    choix = str(input("""1. Combattre
+2. Contourner
+3. Afficher les règles du jeu
+4. Quitter la partie"""))
     if choix == "1":
         def adversaire():
             global winstreak
-            global playerhp
             winstreak = 0
             enemyhp = 4
-            playerhp = 20
+
+            monsternumber += 1
             dice = randint(1, 6)
-            print("Vous tombez face à un adversaire.")
+            print("""Adversaire : numero_adversaire
+            Force de l’adversaire :""", enemyStrength,
+                  """Niveau de vie de l’usager :""", playerhp,
+                  """Combat numero_combat :""", winstreak, """vs""", losestreak)
             print("Vous avez roulé", dice)
             if dice > enemyhp:
                 winstreak += 1
